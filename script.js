9635489599e4f9703afa0db83b17b4e5df5b35d5
Pigtnt <pigtnt284@gmail.com>
2022-08-07 02:56:36 +0800

    a

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 var title = document.getElementById("title");
-var content = document.getElementById("content");
+var Content = document.getElementById("Content");
 var btn = document.getElementById("btn");
 var list = document.getElementById("list");
 
 btn.addEventListener("click", function(){
     if(title.value == "") {
         alert("標題不可為空白");
-    }else if(content.value == ""){
+    }else if(Content.value == ""){
         alert("內容不可為空白");
     }else{
         list.innerHTML = list.innerHTML + `
         <div class="Article">
             <h2>${title.value}</h2>
-            <p>${content.value}</p>
+            <p>${Content.value}</p>
         </div>
         `;
     }
     title.value = "";
-    content.value = ""; 
+    Content.value = ""; 
 })
